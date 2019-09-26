0a9a2b810a550adc9c99452fbc6e08fecadecb44
Panela Leung <leungpanela@gmail.com>
2019-09-26 02:14:47 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,6 @@ class Person
 
   #that accepts a hash upon initialization
   def initialize(attributes)
-    attribuites.each.send {|key,value|("#{attribute=}",value)}
+    attribuites.each. {|key,value|("#{attribute=}",value)}
   end
 end
