3b154e88fb428587d7f850ae2f65263f0d4a0f34
Panela Leung <leungpanela@gmail.com>
2019-09-26 02:21:34 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,7 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    attribuites.each do {|key,value| self.send(("#{key=}"),value)}
+    attribuites.each do |key,value|
+      self.send(("#{key=}"),value)
   end
 end
